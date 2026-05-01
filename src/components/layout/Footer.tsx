1b46b5fc649dd83fa44be399eb83f8507a7133eb
Luis Rosales <conejoconsal@hotmail.com>
2026-05-01 12:16:09 -0600

    ins

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -118,7 +118,7 @@ export default function Footer() {
               </li>
               <li>
                 <a
-                  href="https://sabor-del-barrio.vercel.app/"
+                  href="https://www.instagram.com/sabordelbarrio_7"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-sm text-masa/70 hover:text-masa transition-colors leading-7"
